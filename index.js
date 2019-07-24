547dc445f223c0262a4ad32a0e3c73fc1aee576c
github username <github email address>
2019-07-24 11:30:24 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Your code here
+
+function saturdayFun(string='roller-skate'){
+    return `This Saturday, I want to ${string}!`
+}
+
+function mondayWork(string='go to the office'){
+    return `This Monday, I will ${string}.`
+}
+
+function wrapAdjective(string = '*'){
+    return function(name = 'a hard worker') {
+        return `You are ${string}${name}${string}!`
+    }    
+};
+
+let Calculator = {
+    add: function() {
+        return 1 + 3;
+    },
+    subtract: function() {
+        return 1 - 3;
+    },
+    multiply: function () {
+        return 1 * 3;
+    },
+    divide: function () {
+        return 10 / 5;
+    }
+}
+
+function actionApplyer(integer, array){
+    if(array.length == 0) {
+        return integer;
+      }else {
+        array.forEach(fn => {
+          integer = fn(integer)
+        })
+        return integer;
+      }
+}
